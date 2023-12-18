9b18a2feb3a25dc576bf021457304f9cc0973ce7
imadeekosatria <imadeekosatriawiguna@gmail.com>
2023-12-18 16:08:49 +0800

    fix section part

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -4,6 +4,8 @@ import {
   faChevronDown,
   faCircleArrowUp,
   faMagnifyingGlass,
+  faChevronRight,
+  faPlus,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import TinyChart from "./ui/tiny-chart";
@@ -639,9 +641,28 @@ const Section = () => {
             </div>
           </Card>
           <Card className="min-w-full bg-white h-fit flex gap-x-4">
-              <div className="col-span-5 h-56">
+              <div className="w-2/5 h-56">
                 <CardTitle>List of items to buy</CardTitle>
-                
+                <div className="flex gap-8 items-center mb-4">
+                  <div className="flex flex-col">
+                    <span className="font-medium text-2xl">02:00</span>
+                    <span className="text-slate-300">Sat, August 12</span>
+                  </div>
+                  <FontAwesomeIcon icon={faChevronRight} style={{color: '#e2e8f0'}}/>
+                  <div className="flex flex-col">
+                  <span className="font-medium text-2xl">05:00</span>
+                  <span className="text-slate-300">Sat, September 12</span>
+                  </div>
+                </div>
+                <div className="flex justify-between">
+                  <div className="flex justify-between">
+                    <div className="flex gap-2">
+                    <span className="text-slate-300">0/3</span><span className="font-medium">Shopping list</span>
+                    </div>
+                  </div>
+                  <button className="font-medium flex gap-x-2 items-center"><FontAwesomeIcon icon={faPlus} />Add an item</button>
+                  
+                </div>
               </div>
               <span className="bg-slate-300 h-auto w-0.5 rounded-full"></span>
               <div className="col-span-5">
