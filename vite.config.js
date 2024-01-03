4f54ee94d4cac4103f3ea627337ad396b721ecbe
imadeekosatria <imadeekosatriawiguna@gmail.com>
2024-01-03 11:45:39 +0800

    fix: fix github pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/tyrads-technical-test",
+  base: "/tyrads-technical-test/",
   plugins: [react()],
 })
