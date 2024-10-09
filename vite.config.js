794bc5a5ce7c1b5a051769e853f1a052a7617b81
imadeekosatria <imadeekosatriawiguna@gmail.com>
2024-10-09 12:40:37 +0800

    Update base URL in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/tyrads-technical-test/",
+  base: "/landing-page-vite-react/",
   plugins: [react()],
   build:{
     rollupOptions:{
